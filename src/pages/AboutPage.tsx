5cfe242e24e4b21fe33742a1c1592ba9d047fa9d
Арсений <meqalion2@gmail.com>
2023-03-14 04:16:00 +0300

    :sparkles: feat: создать create и read

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel a saepe earum perferendis placeat aperiam omnis in quo, porro eaque! Eveniet nostrum quibusdam, temporibus accusantium delectus praesentium expedita assumenda. Labore?</div>
+    )
+}
